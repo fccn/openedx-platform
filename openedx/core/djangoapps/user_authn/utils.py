4b3ecdfbb9ec389148de26a068bf14e5cf1917d7
Luis Moreno <luis.moreno@edunext.co>
2020-11-12 14:19:41 -0400

    Adding password police support to generate password.

--- openedx/core/djangoapps/user_authn/utils.py
+++ openedx/core/djangoapps/user_authn/utils.py
@@ -101,6 +101,43 @@ def password_complexity():
     return complexity
 
 
+def password_complexity():
+    """
+    Inspect AUTH_PASSWORD_VALIDATORS setting and generate a dict with the requirements for
+    usage in the generate_password function
+    """
+    password_validators = settings.AUTH_PASSWORD_VALIDATORS
+    known_validators = {
+        "util.password_policy_validators.MinimumLengthValidator": "min_length",
+        "util.password_policy_validators.MaximumLengthValidator": "max_length",
+        "util.password_policy_validators.AlphabeticValidator": "min_alphabetic",
+        "util.password_policy_validators.UppercaseValidator": "min_upper",
+        "util.password_policy_validators.LowerValidator": "min_lower",
+        "util.password_policy_validators.NumericValidator": "min_numeric",
+        "util.password_policy_validators.PunctuationValidator": "min_punctuation",
+        "util.password_policy_validators.SymbolValidator": "min_symbol",
+    }
+    complexity = {}
+
+    for validator in password_validators:
+        param_name = known_validators.get(validator["NAME"], None)
+        if param_name is not None:
+            complexity[param_name] = validator["OPTIONS"].get(param_name, 0)
+
+    # merge alphabetic with lower and uppercase
+    if complexity.get("min_alphabetic") and (
+        complexity.get("min_lower") or complexity.get("min_upper")
+    ):
+        complexity["min_alphabetic"] = max(
+            0,
+            complexity["min_alphabetic"]
+            - complexity.get("min_lower", 0)
+            - complexity.get("min_upper", 0),
+        )
+
+    return complexity
+
+
 def generate_password(length=12, chars=string.ascii_letters + string.digits):
     """Generate a valid random password"""
     if length < 8:
