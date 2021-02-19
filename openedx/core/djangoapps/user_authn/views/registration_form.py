c22534a9d0cdad334a78c7d97212f1fc7cf71bf9
Ivo Branco <ivo.branco@fccn.pt>
2021-02-19 13:05:08 +0000

    Fix use a custom registration field order when using a registration extension form.

--- openedx/core/djangoapps/user_authn/views/registration_form.py
+++ openedx/core/djangoapps/user_authn/views/registration_form.py
@@ -382,7 +382,7 @@ class RegistrationFormFactory:
 
         custom_form = get_registration_extension_form()
         if custom_form:
-            custom_form_field_names = [field_name for field_name, field in custom_form.fields.items()]
+            custom_form_field_names = [ field_name for field_name, field in custom_form.fields.items()]
             valid_fields.extend(custom_form_field_names)
 
         field_order = configuration_helpers.get_value('REGISTRATION_FIELD_ORDER')
@@ -412,15 +412,12 @@ class RegistrationFormFactory:
         Returns:
             HttpResponse
         """
-        form_desc = FormDescription("post", self._get_registration_submit_url(request))
+        form_desc = FormDescription("post", reverse("user_api_registration"))
         self._apply_third_party_auth_overrides(request, form_desc)
 
         # Custom form fields can be added via the form set in settings.REGISTRATION_EXTENSION_FORM
         custom_form = get_registration_extension_form()
-        if custom_form:
-            custom_form_field_names = [field_name for field_name, field in custom_form.fields.items()]
-        else:
-            custom_form_field_names = []
+        custom_form_field_names = [ field_name for field_name, field in custom_form.fields.items()] if custom_form else []
 
         # Go through the fields in the fields order and add them if they are required or visible
         for field_name in self.field_order:
@@ -443,36 +440,26 @@ class RegistrationFormFactory:
                             getattr(custom_form, 'Meta', None), 'serialization_options', {}
                         ).get(field_name, {})
 
-                        field_type = field_options.get('field_type',
-                                                       FormDescription.FIELD_TYPE_MAP.get(field.__class__))
+                        field_type = field_options.get('field_type', FormDescription.FIELD_TYPE_MAP.get(field.__class__))
                         if not field_type:
                             raise ImproperlyConfigured(
-                                "Field type '{}' not recognized for registration extension field '{}'.".format(
+                                u"Field type '{}' not recognized for registration extension field '{}'.".format(
+
                                     field_type,
                                     field_name
                                 )
                             )
                         if self._is_field_visible(field_name) or field.required:
                             form_desc.add_field(
-                                field_name,
-                                label=field.label,
+                                field_name, label=field.label,
                                 default=field_options.get('default'),
-                                field_type=field_options.get('field_type',
-                                                             FormDescription.FIELD_TYPE_MAP.get(field.__class__)),
-                                placeholder=field.initial,
-                                instructions=field.help_text,
-                                required=(self._is_field_required(field_name) or field.required),
+                                field_type=field_options.get('field_type', FormDescription.FIELD_TYPE_MAP.get(field.__class__)),
+                                placeholder=field.initial, instructions=field.help_text, required=(self._is_field_required(field_name) or field.required),
                                 restrictions=restrictions,
                                 options=getattr(field, 'choices', None), error_messages=field.error_messages,
                                 include_default_option=field_options.get('include_default_option'),
                             )
 
-        # remove confirm_email form v1 registration form
-        if is_api_v1(request):
-            for index, field in enumerate(form_desc.fields):
-                if field['name'] == 'confirm_email':
-                    del form_desc.fields[index]
-                    break
         return form_desc
 
     def _get_registration_submit_url(self, request):
