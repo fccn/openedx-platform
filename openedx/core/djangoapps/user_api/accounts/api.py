fccn/edx-platform

Adding the capacity to see and edit nau extension fields on the accounts page

openedx/core/djangoapps/user_api/accounts/api.py
@@ -20,6 +20,7 @@ from student import views as student_views
 from util.model_utils import emit_setting_changed_event
 from util.password_policy_validators import validate_password
 
+from openedx.core.djangoapps.plugins.plugin_extension_points import run_extension_point
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from openedx.core.djangoapps.user_api import errors, accounts, forms, helpers
 from openedx.core.djangoapps.user_api.config.waffle import PREVENT_AUTH_USER_WRITES, SYSTEM_MAINTENANCE_MSG, waffle
@@ -245,6 +246,13 @@ def update_account_settings(requesting_user, update, username=None):
                 meta[field_name] = new_value
             existing_user_profile.set_meta(meta)
             existing_user_profile.save()
+
+        # Allow a plugin to save the updated values
+        run_extension_point(
+            'NAU_STUDENT_ACCOUNT_PARTIAL_UPDATE',
+            update=update,
+            user=existing_user,
+        )
 
     except PreferenceValidationError as err:
         raise AccountValidationError(err.preference_errors)

fccn/openedx-platform

openedx/core/djangoapps/user_api/accounts/api.py
@@ -28,6 +28,7 @@ from lms.djangoapps.certificates.api import get_certificates_for_user
 from lms.djangoapps.certificates.data import CertificateStatuses
 
 from openedx.core.djangoapps.enrollments.api import get_verified_enrollments
+from openedx.core.djangoapps.plugins.plugin_extension_points import run_extension_point
 from openedx.core.djangoapps.user_api import accounts, errors, helpers
 from openedx.core.djangoapps.user_api.errors import (
     AccountUpdateError,
@@ -168,6 +169,13 @@ def update_account_settings(requesting_user, update, username=None):
         _store_old_name_if_needed(old_name, user_profile, requesting_user)
         _update_extended_profile_if_needed(update, user_profile)
         _update_state_if_needed(update, user_profile)
+
+        # Allow a plugin to save the updated values
+        run_extension_point(
+            'NAU_STUDENT_ACCOUNT_PARTIAL_UPDATE',
+            update=update,
+            user=user,
+        )
 
     except PreferenceValidationError as err:
         raise AccountValidationError(err.preference_errors)  # lint-amnesty, pylint: disable=raise-missing-from